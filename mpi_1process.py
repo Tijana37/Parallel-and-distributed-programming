650f3ef2910e00b71831c9e14dd98a300644df23
Tijana37 <79831305+Tijana37@users.noreply.github.com>
2022-07-28 23:26:06 +0200

    Add files via upload

--- mpi_1process.py
+++ mpi_1process.py
@@ -11,6 +11,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Conv2D,MaxPooling2D, Activation, Flatten, Dropout, Dense
 from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing.image import img_to_array
+import tensorflow.keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 #import matplotlib.pyplot as plt
@@ -18,15 +19,19 @@ import time
 import csv
 
 
-EPOCHS = 50
+directory_root = 'C:/Users/Tijana Atanasovska/PycharmProjects/plants_detection/plants_data'
+EPOCHS = 25
 INIT_LR = 1e-3
 BS = 32
 default_image_size = tuple((128, 128))
 image_size = 0
-directory_root = 'C:/Users/Tijana Atanasovska/PycharmProjects/plants_detection/plants_data'
 width=128
 height=128
 depth=3
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
 
 def convert_image_to_array(image_dir):
@@ -42,18 +47,23 @@ def convert_image_to_array(image_dir):
         return None
 
 
-def convert_images():
+def convert_images(from_range, to_range):
     image_list, label_list = [], []
     #try:
     print("[INFO] Loading images ...")
     root_dir = listdir(directory_root)
+    print(root_dir)
     for directory in root_dir:
         # remove .DS_Store from list
         if directory == ".DS_Store":
             root_dir.remove(directory)
 
     for plant_folder in root_dir:
-        plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")
+        print(listdir(f"{directory_root}/{plant_folder}"))
+        plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")[from_range:to_range]
+
+
+
 
         for disease_folder in plant_disease_folder_list:
             # remove .DS_Store from list
@@ -72,9 +82,6 @@ def convert_images():
             for image in plant_disease_image_list[:200]:
                 image_directory = f"{directory_root}/{plant_folder}/{plant_disease_folder}/{image}"
                 if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
-
-                    #np.savetxt(f,convert_image_to_array(image_directory), delimiter=',' )
-
                     image_list.append(convert_image_to_array(image_directory))
                     label_list.append(plant_disease_folder)
 
@@ -93,7 +100,6 @@ def labelizer(label_list):
     n_classes = len(label_binarizer.classes_)
 
     return image_labels, n_classes
-
 
 def model(n_classes):
     model = Sequential()
@@ -131,91 +137,74 @@ def model(n_classes):
     model.add(Dense(n_classes))
     model.add(Activation("softmax"))
 
-    print(model.summary())
+    #print(model.summary())
 
     return model
 
-if __name__ == '__main__':
-    gpus = tensorflow.config.experimental.list_physical_devices('GPU')
-    print(gpus)
-    tensorflow.config.experimental.set_memory_growth(gpus[0], True)
-    print(tensorflow.device('/device:gpu:0'))
 
-    with tensorflow.device('/device:gpu:0'):
+def createmodel():
+
+    print("creating model.......")
+    aug = ImageDataGenerator(
+        rotation_range=25, width_shift_range=0.1,
+        height_shift_range=0.1, shear_range=0.2,
+        zoom_range=0.2, horizontal_flip=True,
+        fill_mode="nearest")
+
+    model_ = model(n_classes=15)
+    opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
+
+    model_.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+    return model_, aug
+
+if rank == 0:
+    print("Running process: ", comm.rank)
+
+    time_start = time.time()
+    image_list, label_list = convert_images(0,15)
+    time_end = time.time()
+    print("Time to process all data - rank 0: ", time_end - time_start)
+
+    time_start = time.time()
+    np_image_list = np.array(image_list, dtype=np.float16) / 225.0
+    time_end = time.time()
+    print("Time to convert data to numpy arrays- rank 0: ", time_end - time_start)
+
+    image_labels, n_classes = labelizer(label_list)
+
+    print("[INFO] Splitting full data to train, test....")
+    x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, \
+                                                        test_size=0.2, shuffle=True, random_state=42)
+    model_, aug = createmodel()
+    # train the network
+    print("[INFO] training network...")
+
+    #for cycle to train the network so we can average weights from both tasks and update before every epoch
+    time_full_training_s = time.time()
+
+    with tensorflow.device('/cpu:0'):
         time_start = time.time()
-        image_list, label_list = convert_images()
+        history = model_.fit_generator(
+                aug.flow(x_train, y_train, batch_size=BS),
+                validation_data=(x_test, y_test),
+                steps_per_epoch=len(x_train) // BS,
+                epochs=EPOCHS, verbose=1
+            )
         time_end = time.time()
-        print("Time to process all data - GPU: ", time_end - time_start)
+        print(f"Time to train 25 epochs - rank 0: ", time_end - time_start)
 
-        image_labels, n_classes = labelizer(label_list)
-
-        time_start = time.time()
-        np_image_list = np.array(image_list, dtype=np.float16) / 225.0
-        time_end = time.time()
-        print("Time to convert all data to numpy arrays - GPU: ", time_end - time_start)
-
-        print("[INFO] Spliting data to train, test")
-        x_train, x_test, y_train, y_test = train_test_split(np_image_list, image_labels, test_size=0.2, random_state=42)
-
-        aug = ImageDataGenerator(
-            rotation_range=25, width_shift_range=0.1,
-            height_shift_range=0.1, shear_range=0.2,
-            zoom_range=0.2, horizontal_flip=True,
-            fill_mode="nearest")
-
-        model = model(15)
-
-        opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
-        # distribution
-        model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-        # train the network
-        print("[INFO] training network...")
-
-        time_start = time.time()
-        history = model.fit_generator(
-            aug.flow(x_train, y_train, batch_size=BS),
-            validation_data=(x_test, y_test),
-            steps_per_epoch=len(x_train) // BS,
-            epochs=EPOCHS, verbose=1
-        )
-        time_end = time.time()
-        print("Time to process 25 epochs - GPU: ", time_end - time_start)
-
+            #Wait for weights of the other task; sum and average the weights
 
         acc = history.history['accuracy']
         val_acc = history.history['val_accuracy']
         loss = history.history['loss']
         val_loss = history.history['val_loss']
         epochs = range(1, len(acc) + 1)
-        with open('results_gpu.csv', 'a', encoding='UTF8') as f:
+        with open('results_1process.csv', 'a', encoding='UTF8') as f:
             writer = csv.writer(f)
             # write the data
             writer.writerow(acc)
             writer.writerow(val_acc)
             writer.writerow(loss)
             writer.writerow(val_loss)
-
-        # # Train and validation accuracy
-        # plt.plot(epochs, acc, 'b', label='Training accurarcy')
-        # plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
-        # plt.title('Training and Validation accurarcy')
-        # plt.legend()
-        #
-        # plt.figure()
-        # # Train and validation loss
-        # plt.plot(epochs, loss, 'b', label='Training loss')
-        # plt.plot(epochs, val_loss, 'r', label='Validation loss')
-        # plt.title('Training and Validation loss')
-        # plt.legend()
-        # plt.show()
-        #
-        # print("[INFO] Calculating model accuracy")
-        # scores = model.evaluate(x_test, y_test)
-        # print(f"Test Accuracy: {scores[1] * 100}")
-        #
-        # # save the model to disk
-        # print("[INFO] Saving model...")
-        # pickle.dump(model, open('cnn_model.pkl', 'wb'))
-        #
-        #
-        #
--- mpi_doc.py
+++ mpi_doc.py
@@ -1,39 +1,32 @@
 from mpi4py import MPI
-import cv2
-from keras.preprocessing.image import img_to_array
-import numpy as np
-from os import listdir
 import numpy as np
 import pickle
 import cv2
 from os import listdir
 from sklearn.preprocessing import LabelBinarizer
-from keras.models import Sequential
-from keras.layers import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation, Flatten, Dropout, Dense
-from keras import backend as K
-from keras.preprocessing.image import img_to_array
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 import tensorflow
-import time
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization, Conv2D,MaxPooling2D, Activation, Flatten, Dropout, Dense
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import img_to_array
+import tensorflow.keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
-from tensorflow.python.keras.optimizer_v2 import adam
-from keras.optimizers import adam_v2
+#import matplotlib.pyplot as plt
+import time
+import csv
 
 
 directory_root = 'C:/Users/Tijana Atanasovska/PycharmProjects/plants_detection/plants_data'
 EPOCHS = 25
 INIT_LR = 1e-3
 BS = 32
-default_image_size = tuple((256, 256))
+default_image_size = tuple((128, 128))
 image_size = 0
-width=256
-height=256
+width=128
+height=128
 depth=3
 
 
@@ -68,9 +61,6 @@ def convert_images(from_range, to_range):
     for plant_folder in root_dir:
         print(listdir(f"{directory_root}/{plant_folder}"))
         plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")[from_range:to_range]
-
-
-
 
         for disease_folder in plant_disease_folder_list:
             # remove .DS_Store from list
@@ -159,7 +149,7 @@ def createmodel():
         fill_mode="nearest")
 
     model_ = model(n_classes=15)
-    opt = adam_v2.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
+    opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
 
     model_.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
@@ -168,65 +158,74 @@ def createmodel():
 if rank == 0:
     print("Running process: ", comm.rank)
 
-
     time_start = time.time()
     image_list, label_list = convert_images(0,8)
     time_end = time.time()
     print("Time to process half data - rank 0: ", time_end - time_start)
+
     time_start = time.time()
     np_image_list = np.array(image_list, dtype=np.float16) / 225.0
-
     time_end = time.time()
-    print("Time to convert all data to numpy arrays- rank 0: ", time_end - time_start)
+    print("Time to convert data to numpy arrays- rank 0: ", time_end - time_start)
 
     send_data = {'image_list0': np_image_list[:], 'label_list0': label_list[:]}
-    print("Sending image list and label list......")
     comm.send(send_data, dest=1, tag=11)
+    print("Sending image list and label list......")
 
     x_train_second, x_test_second, y_train_second, y_test_second = comm.recv(source=1, tag=11)
-
     print("Received shuffled data ......")
-    #
-    # model_, aug = createmodel()
-    # # train the network
-    # # print("[INFO] training network...")
-    #
-    # with tensorflow.device('/cpu:0'):
-    #     history = model_.fit_generator(
-    #             aug.flow(x_train_second, y_train_second, batch_size=BS),
-    #             validation_data=(x_test_second, y_test_second),
-    #             steps_per_epoch=len(x_train_second) // BS,
-    #             epochs=10, verbose=1
-    #         )
-    # #
-    #     print("Trained")
-    #     acc = history.history['accuracy']
-    #     val_acc = history.history['val_accuracy']
-    #     loss = history.history['loss']
-    #     val_loss = history.history['val_loss']
-    #     epochs = range(1, len(acc) + 1)
-    #     # Train and validation accuracy
-    #     plt.plot(epochs, acc, 'b', label='Training accurarcy')
-    #     plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
-    #     plt.title('Training and Validation accurarcy')
-    #     plt.legend()
-    #
-    #     plt.figure()
-    #     # Train and validation loss
-    #     plt.plot(epochs, loss, 'b', label='Training loss')
-    #     plt.plot(epochs, val_loss, 'r', label='Validation loss')
-    #     plt.title('Training and Validation loss')
-    #     plt.legend()
-    #     plt.show()
 
+    model_, aug = createmodel()
+    # train the network
+    print("[INFO] training network...")
 
-#print(model_.get_weights())
-    # w1 = model_.get_weights()
-    # print(w1[0])
-    # w2 = model_.get_weights()
-    # w =  (w1 + w2) / 2
-    # print(w[0])
+    #for cycle to train the network so we can average weights from both tasks and update before every epoch
+    time_full_training_s = time.time()
+    for i in range(EPOCHS):
+        with tensorflow.device('/cpu:0'):
+            time_start = time.time()
+            history = model_.fit_generator(
+                    aug.flow(x_train_second, y_train_second, batch_size=BS),
+                    validation_data=(x_test_second, y_test_second),
+                    steps_per_epoch=len(x_train_second) // BS,
+                    epochs=1, verbose=1
+                )
+            time_end = time.time()
+            print(f"Time to train epoch {i} - rank 0: ", time_end - time_start)
 
+            #Wait for weights of the other task; sum and average the weights
+            w = comm.recv(source=1, tag=11)
+            avg_weights = np.add(w , model_.get_weights())
+            avg_weights = np.divide(avg_weights, 2)
+
+            comm.send(avg_weights, dest=1, tag=11)
+            model_.set_weights(avg_weights)
+
+            acc = history.history['accuracy']
+            val_acc = history.history['val_accuracy']
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+            epochs = range(1, len(acc) + 1)
+            data = ['Task 0', str('Epoch')+str(i),acc[0], val_acc[0], loss[0], val_loss[0]]
+            with open('results.csv', 'a', encoding='UTF8') as f:
+                writer = csv.writer(f)
+                # write the data
+                writer.writerow(data)
+        # Train and validation accuracy
+        # plt.plot(epochs, acc, 'b', label='Training accurarcy')
+        # plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+        # plt.title('Training and Validation accurarcy')
+        # plt.legend()
+        #
+        # plt.figure()
+        # # Train and validation loss
+        # plt.plot(epochs, loss, 'b', label='Training loss')
+        # plt.plot(epochs, val_loss, 'r', label='Validation loss')
+        # plt.title('Training and Validation loss')
+        # plt.legend()
+        # plt.show()
+
+    print("Time full training ", time.time() - time_full_training_s)
 
 elif rank == 1:
     print("Running process: ", comm.rank)
@@ -236,22 +235,23 @@ elif rank == 1:
     time_end = time.time()
     print("Time to process half data - rank 1: ", time_end - time_start)
 
-
+    time_start = time.time()
     np_image_list = np.array(image_list, dtype=np.float16) / 225.0
+    time_end = time.time()
+    print("Time to convert data to numpy arrays- rank 1: ", time_end - time_start)
 
     received_data = comm.recv(source=0, tag=11)
-
+    print("Received image list and label list..........")
+    #concate data from both tasks to shuffle
     np_image_list_full = np.concatenate((np_image_list, received_data['image_list0']), axis=0)
-
     np_image_labels_full = np.concatenate((label_list, received_data['label_list0']), axis=0)
 
     #labelizer must be on the whole dataset to make binary encoder for 15 classes,not 7 and 8 separetely
     image_labels, n_classes = labelizer(np_image_labels_full)
 
-    print("Received image list and label list..........")
-
     print("[INFO] Splitting full data to train, test....")
-    x_train, x_test, y_train, y_test = train_test_split(np_image_list_full, image_labels, test_size=0.2, shuffle=True, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(np_image_list_full, image_labels,\
+                                                        test_size=0.2, shuffle=True, random_state=42)
 
     x_train_first, x_test_first, y_train_first, y_test_first = x_train[:len(x_train)//2], x_test[:len(x_test)//2],\
                                                                y_train[:len(y_train)//2], y_test[:len(y_test)//2]
@@ -265,39 +265,31 @@ elif rank == 1:
 
     model_, aug = createmodel()
     # # train the network
-    # print("[INFO] training network...")
+    print("[INFO] training network...")
 
-    time_start = time.time()
+    for i in range(EPOCHS):
+        with tensorflow.device('/cpu:0'):
+            time_start = time.time()
+            history = model_.fit_generator(
+                aug.flow(x_train_first, y_train_first, batch_size=BS),
+                validation_data=(x_test_first, y_test_first),
+                steps_per_epoch=len(x_train_first) // BS,
+                epochs=1, verbose=1
+            )
+            time_end = time.time()
+            print(f"Time to train epoch {i} - rank 1: ", time_end - time_start)
 
-    with tensorflow.device('/cpu:0'):
-        history = model_.fit_generator(
-            aug.flow(x_train_first, y_train_first, batch_size=BS),
-            validation_data=(x_test_first, y_test_first),
-            steps_per_epoch=len(x_train_second) // BS,
-            epochs=1, verbose=1
-        )
-        time_end = time.time()
-        print("Time to train 1 epoch half data - rank 1: ", time_end - time_start)
-    #
-        print("Trained")
-    #     acc = history.history['accuracy']
-    #     val_acc = history.history['val_accuracy']
-    #     loss = history.history['loss']
-    #     val_loss = history.history['val_loss']
-    #     epochs = range(1, len(acc) + 1)
-    #     # Train and validation accuracy
-    #     plt.plot(epochs, acc, 'b', label='Training accurarcy')
-    #     plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
-    #     plt.title('Training and Validation accurarcy')
-    #     plt.legend()
-    #
-    #     plt.figure()
-    #     # Train and validation loss
-    #     plt.plot(epochs, loss, 'b', label='Training loss')
-    #     plt.plot(epochs, val_loss, 'r', label='Validation loss')
-    #     plt.title('Training and Validation loss')
-    #     plt.legend()
-    #     plt.show()
-    #
-    #
-    #
+            comm.send(model_.get_weights(), dest=0, tag=11)
+            avg_weights = comm.recv(source=0, tag=11)
+            model_.set_weights(avg_weights)
+
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        epochs = range(1, len(acc) + 1)
+        data = ['Task 1', str('Epoch') + str(i), acc[0], val_acc[0], loss[0], val_loss[0]]
+        with open('results.csv', 'a', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            # write the data
+            writer.writerow(data)
